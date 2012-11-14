760e960cdd1a870821b549874a148c9fab511655
Ali Sabil <ali.sabil@soundrop.com>
2012-11-14 12:22:04 +0100

    Only install the error logger if configured properly

--- src/raven_app.erl
+++ src/raven_app.erl
@@ -8,8 +8,16 @@
 
 
 start(_StartType, _StartArgs) ->
-	error_logger:add_report_handler(raven_error_logger),
-	raven_sup:start_link().
+	case application:get_env(uri) of
+		{ok, _} ->
+			case application:get_env(error_logger) of
+				{ok, true} -> error_logger:add_report_handler(raven_error_logger);
+				_ -> ok
+			end,
+			raven_sup:start_link();
+		_ ->
+			{error, missing_configuration}
+	end.
 
 stop(_State) ->
 	ok.
